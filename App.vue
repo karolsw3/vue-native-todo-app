38cc6fc1b642fab3c4199ba55e2a20be1fb80954
Karol Świerczek <karol@swierczek.pro>
2019-03-30 15:11:04 +0100

    add basic version of the app

---
+++ App.vue
@@ -0,0 +1,192 @@
+<template>
+  <view class="container">
+    <scroll-view class="todo-list" :class="newTodoScreen ? 'dimmed':''">
+      <text class="text-header">Your TODO list</text>
+      <todo-item
+        v-for="(todo, index) in todoList"
+        :index="index"
+        :text="todo.text"
+        :onPress="markTodoAsDone"
+        :key="index"></todo-item>
+      <view v-if="doneList.length > 0">
+        <text class="text-header">Done</text>
+        <todo-item
+          v-for="(todo, index) in doneList"
+          :text="todo.text"
+          :index="index"
+          :onPress="()=>{}"
+          :key="index"
+          :done="true">
+        </todo-item>
+        <touchable-opacity
+          :onPress="() => { doneList = []}"
+          :style="{ justifySelf: 'center', alignItems: 'center', marginVertical: 10 }">
+          <text>Clear all</text>
+        </touchable-opacity>
+        <view class="hollow-brick"></view>
+      </view>
+    </scroll-view>
+    <view
+      class="addItemContainer"
+      :class="newTodoScreen ? '':'hidden'"
+      :style="{shadowOffset: {height: 0, width: 0}}">
+      <text class="text-header">Add new todo</text>
+      <text-input
+        class="text-input"
+        placeholder="Buy a piano"
+        v-model="newTodoText"
+      />
+      <touchable-opacity 
+        title="add"
+        class="button2"
+        :onPress="()=> { addTodo() }">
+        <text class="text-button">Add</text>
+      </touchable-opacity>
+    </view>
+    <touchable-opacity
+      class="addItemButton"
+      :class="newTodoScreen ? 'rotated':''"
+      title="add" 
+      :onPress="()=>{ newTodoScreen = !newTodoScreen }">
+      <image :source="plusIcon" class="image-plus"></image>
+    </touchable-opacity>
+  </view>
+</template>
+<script>
+import TodoItem from './components/TodoItem'
+import plusIcon from './assets/add.png'
+
+export default {
+  data: function () {
+    return {
+      plusIcon,
+      todoList: [],
+      doneList: [],
+      newTodoScreen: false,
+      newTodoText: ''
+    }
+  },
+  components: {
+    TodoItem
+  },
+  methods: {
+    markTodoAsDone (index) {
+      this.doneList.push(this.todoList[index])
+      this.todoList.splice(index, 1)
+    },
+    addTodo () {
+      if (this.newTodoText !== '') {
+        this.todoList.push({text: this.newTodoText})
+        this.newTodoText = ''
+        this.newTodoScreen = false
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+
+.text-color-primary {
+  color: white;
+}
+
+.addItemButton {
+  width: 70px;
+  height: 70px;
+  background-color: #FF3352;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+  position: absolute;
+  bottom: 30px;
+  border-radius: 100px;
+  z-index: 3;
+}
+
+.button2 {
+  align-self: center;
+  border-radius: 5px;
+  padding-horizontal: 30px;
+  padding-vertical: 12px;
+  background-color: #f7f7f7;
+}
+
+.text-button {
+  color: #222;
+  font-size: 18px;
+  font-weight: bold;
+}
+
+.addItemContainer {
+  width: 95%;
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 10px;
+  position: absolute;
+  top: 170px;
+  z-index: 2;
+  shadow-color: black;
+  shadow-opacity: 0.16;
+  shadow-radius: 5px;
+}
+
+.text-input {
+  margin-vertical: 20px;
+  color: #222;
+  padding: 20px;
+  background-color: #f7f7f7;
+  border-radius: 12px;
+  font-size: 21px;
+}
+
+.hidden {
+  display: none;
+}
+
+.dimmed {
+  opacity: 0.12;
+}
+
+.rotated {
+  transform: rotate(45deg)
+}
+
+.todo-list {
+  padding-top: 60px;
+  flex: 1;
+  width: 100%;
+  padding-horizontal: 20px;
+}
+
+.button {
+  font-size: 16px;
+  color: black;
+  font-weight: 600;
+}
+
+.hollow-brick {
+  width: 100%;
+  height: 180px;
+}
+
+.image-plus {
+  width: 40px;
+  height: 40px;
+}
+
+.text-header {
+  text-align: left;
+  align-self: flex-start;
+  margin-bottom: 15px;
+  font-size: 26px;
+  font-weight: bold;
+}
+
+</style>
---
+++ components/TodoItem.vue
@@ -0,0 +1,65 @@
+<template>
+  <view class="todoItem">
+    <text class="todo-text">{{text}}</text>
+    <touchable-opacity :on-press="() => {onPress(index)}">
+      <image
+        :source="CheckIcon"
+        class="check-icon"
+        :class="done ? 'check-icon--done':'check-icon--normal'">
+      </image>
+    </touchable-opacity>
+  </view>
+</template>
+<script>
+import CheckIcon from '../assets/checked.png'
+
+export default {
+  props: {
+    text: String,
+    index: Number,
+    done: {
+      type: Boolean,
+      required: false
+    },
+    onPress: Function
+  },
+  data: function () {
+    return {
+      CheckIcon
+    }
+  }
+}
+</script>
+
+<style>
+.todoItem {
+  width: 100%;
+  align-self: stretch;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 20px;
+  margin-bottom: 15px;
+  background-color: #f7f7f7;
+  border-radius: 12px;
+}
+
+.todo-text {
+  font-size: 21px;
+  font-weight: 400;
+}
+
+.check-icon {
+  align-self: flex-end;
+  height: 25px;
+  width: 25px;
+}
+
+.check-icon--normal {
+  opacity: 0.2;
+}
+
+.check-icon--done {
+  opacity: 0.9;
+}
+
+</style>
